754efb9faff9f0fc920349fcbd40a66d612629f4
Isaac Elmer <mytigey@gmail.com>
2026-07-16 14:31:15 -0600

    More stuff

--- common/src/main/java/isaacutah1379/mods/redstoneextended/block/ClockCircuitBlock.java
+++ common/src/main/java/isaacutah1379/mods/redstoneextended/block/ClockCircuitBlock.java
@@ -1,10 +1,19 @@
 package isaacutah1379.mods.redstoneextended.block;
 
+import isaacutah1379.mods.redstoneextended.block.entity.ClockCircuitBlockEntity;
+import isaacutah1379.mods.redstoneextended.block.entity.REBlockEntities;
+import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.world.item.context.BlockPlaceContext;
+import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.BaseEntityBlock;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.DirectionalBlock;
+import net.minecraft.world.level.block.RenderShape;
+import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.block.entity.BlockEntityTicker;
+import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
@@ -29,5 +38,32 @@ public class ClockCircuitBlock extends BaseEntityBlock {
 
     public BlockState getStateForPlacement(BlockPlaceContext context) {
         return this.defaultBlockState().setValue(FACING, context.getNearestLookingDirection());
+    }
+
+    public BlockEntity newBlockEntity(BlockPos position, BlockState state) {
+        return new ClockCircuitBlockEntity(position, state);
+    }
+
+    public RenderShape getRenderShape(BlockState state) {
+        return RenderShape.MODEL;
+    }
+
+    public boolean isSignalSource(BlockState state) {
+        return true;
+    }
+
+    public int getSignal(BlockState state, BlockGetter block, BlockPos pos, Direction direction) {
+        return this.getDirectSignal(state, block, pos, direction);
+    }
+
+    public int getDirectSignal(BlockState state, BlockGetter blockGetter, BlockPos pos, Direction direction) {
+        return state.getValue(POWERED) && direction == state.getValue(FACING).getOpposite() ? 15 : 0;
+    }
+
+    public <T extends BlockEntity> BlockEntityTicker<T> getTicker(Level level, BlockState state, BlockEntityType<T> type) {
+        if (!level.isClientSide()) {
+            return createTickerHelper(type, REBlockEntities.clockCircuitBlockEntity.get(), ClockCircuitBlockEntity::tick);
+        }
+        return null;
     }
 }
